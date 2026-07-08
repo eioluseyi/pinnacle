a35406596d015dd4f9b68dfebbf88123f3de8756
Emmanuel Imolorhe <imolorhe@gmail.com>
2026-07-08 07:10:25 +0100

    Update package.json to change main entry point to 'electron/main.js' and refactor start script for concurrent execution with 'electron-forge'. Introduced 'desktop' script for development workflow.

---
+++ electron/main.js
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from 'electron/main';
+// import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 960,
+    height: 700,
+    // webPreferences: {
+    //   preload: path.join(__dirname, 'preload.js'),
+    //   contextIsolation: true,
+    //   nodeIntegration: false,
+    // },
+  });
+
+  win.loadURL('http://localhost:3000');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
